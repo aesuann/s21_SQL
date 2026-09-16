f4dad07ec12b4b709cd01e930fc5fb1d8a9eff90
Rin <rin@Rins-MacBook-Air.local>
2026-09-16 19:43:34 +0300

    done

--- src/ex01/day00_ex01.sql
+++ src/ex01/day00_ex01.sql
@@ -1,9 +1,6 @@
 SELECT name, age
 FROM person
-<<<<<<< HEAD
 WHERE address = 'Kazan' AND gender = 'female'
-=======
 WHERE address = 'Kazan'
 AND gender = 'female'
->>>>>>> 99ab783f465306e71e97c38fe89e2c0fb81a6936
 ORDER BY name;
